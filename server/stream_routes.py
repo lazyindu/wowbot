256d67a6de314418acb86da60a2ce9c40100119f
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-17 00:43:16 +0530

    Update stream_routes.py

--- server/stream_routes.py
+++ server/stream_routes.py
@@ -18,25 +18,6 @@ from util.render_template import render_page
 from info import *
 
 
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(_):
-    return web.json_response(
-        {
-            "server_status": "running",
-            "uptime": get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + StreamBot.username,
-            "connected_bots": len(multi_clients),
-            "loads": dict(
-                ("bot" + str(c + 1), l)
-                for c, (_, l) in enumerate(
-                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
-                )
-            ),
-            "version": __version__,
-        }
-    )
 
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
