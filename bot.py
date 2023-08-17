38060b307ce9f79f7cb624c912545543e9defac4
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-17 23:11:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,26 +2,37 @@ import logging
 import logging.config
 
 # Get logging configurations
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
+import sys
 import asyncio
 import glob
+import importlib
 from lazybot import StreamBot
-from pyrogram import Client, __version__
+from pathlib import Path
+from util.keepalive import ping_server
+from lazybot.clients import initialize_clients
+
+from pyrogram import Client, __version__ , idle
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import *
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from aiohttp import web
-from plugins import web_server
+from server import web_server
 
 PORT = "8080"
+
 
 class Bot(Client):
 
@@ -99,21 +110,66 @@ class Bot(Client):
 
 
 app = Bot()
-app = Bot()
+
+ppath = "plugins/*.py"
+files = glob.glob(ppath)
+StreamBot.start()
+loop = asyncio.get_event_loop()
 async def start_services():
-    await app.start()
+    print('\n')
+    print('------------------- Initalizing Telegram Bot -------------------')
+    bot_info = await StreamBot.get_me()
+    StreamBot.username = bot_info.username
+    print("------------------------------ DONE ------------------------------")
+    print()
+    print(
+        "---------------------- Initializing Clients ----------------------"
+    )
+    await initialize_clients()
+    print("------------------------------ DONE ------------------------------")
+    print('\n')
+    print('--------------------------- Importing ---------------------------')
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem.replace(".py", "")
+            plugins_dir = Path(f"Adarsh/bot/plugins/{plugin_name}.py")
+            import_path = ".plugins.{}".format(plugin_name)
+            spec = importlib.util.spec_from_file_location(import_path, plugins_dir)
+            load = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(load)
+            sys.modules["Adarsh.bot.plugins." + plugin_name] = load
+            print("Imported => " + plugin_name)
+    if ON_HEROKU:
+        print("------------------ Starting Keep Alive Service ------------------")
+        print()
+        asyncio.create_task(ping_server())
+    print('-------------------- Initalizing Web Server -------------------------')
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0" if ON_HEROKU else BIND_ADRESS
+    await web.TCPSite(app, bind_address, PORT).start()
+    print('----------------------------- DONE ---------------------------------------------------------------------')
+    print('\n')
+    print('---------------------------------------------------------------------------------------------------------')
+    print('---------------------------------------------------------------------------------------------------------')
+    print(' follow me for more such exciting bots! https://github.com/adarsh-goel')
+    print('---------------------------------------------------------------------------------------------------------')
+    print('\n')
+    print('----------------------- Service Started -----------------------------------------------------------------')
+    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
+    print('                        server ip =>> {}:{}'.format(bind_address, PORT))
+    if ON_HEROKU:
+        print('                        app runnng on =>> {}'.format(FQDN))
+    print('---------------------------------------------------------------------------------------------------------')
+    print('---------------------------------------------------------------------------------------------------------')
+    await idle()
 
 async def stop_services():
     await app.stop()
 
 if __name__ == '__main__':
-    ppath = "Adarsh/bot/plugins/*.py"
-    files = glob.glob(ppath)
-    StreamBot.start()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_services())
     try:
-        loop.run_forever()
+        loop.run_until_complete(start_services())
     except KeyboardInterrupt:
-        loop.run_until_complete(stop_services())
-        loop.close()
+        logging.info('----------------------- Service Stopped -----------------------')
