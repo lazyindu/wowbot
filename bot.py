6f6a07a2d12611bc58c03716fb4dd23b9d8b9fca
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-18 23:27:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,109 +31,92 @@ from util.keepalive import ping_server
 from lazybot.clients import initialize_clients
 
 PORT = "8080"
+StreamBot.start()
+loop = asyncio.get_event_loop()
 
-class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            name=SESSION,
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
-        )
 
-    async def start(self):
-        await super().start()
-        print('\n')
-        print('------------------- Initalizing Telegram Bot -------------------')
-        bot_info = await self.get_me()
-        StreamBot.username = bot_info.username
-        print("------------------------------ DONE ------------------------------")
+
+async def iter_messages(
+    client,
+    chat_id: Union[int, str],
+    limit: int,
+    offset: int = 0,
+) -> Optional[AsyncGenerator["types.Message", None]]:
+    """Iterate through a chat sequentially.
+    This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+    you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+    single call.
+    Parameters:
+        client (:obj:`pyrogram.Client`):
+            The Pyrogram client instance.
+            
+        chat_id (``int`` | ``str``):
+            Unique identifier (int) or username (str) of the target chat.
+            For your personal cloud (Saved Messages) you can simply use "me" or "self".
+            For a contact that exists in your Telegram address book you can use his phone number (str).
+            
+        limit (``int``):
+            Identifier of the last message to be returned.
+            
+        offset (``int``, *optional*):
+            Identifier of the first message to be returned.
+            Defaults to 0.
+    Returns:
+        ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+    Example:
+        .. code-block:: python
+            for message in iter_messages(client, "pyrogram", 1, 15000):
+                print(message.text)
+    """
+    current = offset
+    while True:
+        new_diff = min(200, limit - current)
+        if new_diff <= 0:
+            return
+        messages = await client.get_messages(chat_id, list(range(current, current + new_diff + 1)))
+        for message in messages:
+            yield message
+            current += 1
+
+async def start_services():
+    await StreamBot.start()
+    print('\n')
+    print('------------------- Initalizing Telegram Bot -------------------')
+    bot_info = await StreamBot.get_me()
+    StreamBot.username = bot_info.username
+    print("------------------------------ DONE ------------------------------")
+    print()
+    print(
+        "---------------------- Initializing Clients ----------------------"
+    )
+    await initialize_clients()
+    print("------------------------------ DONE ------------------------------")
+    print('\n')
+
+    if ON_HEROKU:
+        print("------------------ Starting Keep Alive Service ------------------")
         print()
-        print(
-            "---------------------- Initializing Clients ----------------------"
-        )
-        await initialize_clients()
-        print("------------------------------ DONE ------------------------------")
-        print('\n')
-
-        if ON_HEROKU:
-            print("------------------ Starting Keep Alive Service ------------------")
-            print()
-            asyncio.create_task(ping_server())
-        print('-------------------- Initalizing Web Server -------------------------')
-        b_users, b_chats = await db.get_banned()
-        temp.BANNED_USERS = b_users
-        temp.BANNED_CHATS = b_chats
-        await Media.ensure_indexes()
-        me = await self.get_me()
-        temp.ME = me.id
-        temp.U_NAME = me.username
-        temp.B_NAME = me.first_name
-        self.username = '@' + me.username
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0" if ON_HEROKU else BIND_ADRESS
-        await web.TCPSite(app, bind_address, PORT).start()
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        logging.info(LOG_STR)
-        await idle()
-
-
-    async def stop(self, *args):
-        await super().stop()
-        logging.info("Bot stopped. Bye.")
-
-
-    async def iter_messages(
-        self,
-        chat_id: Union[int, str],
-        limit: int,
-        offset: int = 0,
-    ) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
-        current = offset
-        while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
-                current += 1
-
-
-def main():
-    loop = asyncio.get_event_loop()
-    if not loop.is_running():
-        asyncio.run(Bot().start())
+        asyncio.create_task(ping_server())
+    print('-------------------- Initalizing Web Server -------------------------')
+    b_users, b_chats = await db.get_banned()
+    temp.BANNED_USERS = b_users
+    temp.BANNED_CHATS = b_chats
+    await Media.ensure_indexes()
+    me = await StreamBot.get_me()
+    temp.ME = me.id
+    temp.U_NAME = me.username
+    temp.B_NAME = me.first_name
+    StreamBot.username = '@' + me.username
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0" if ON_HEROKU else BIND_ADRESS
+    await web.TCPSite(app, bind_address, PORT).start()
+    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+    logging.info(LOG_STR)
+    await idle()
 
 if __name__ == '__main__':
     try:
-        main()
+        loop.run_until_complete(start_services())
     except KeyboardInterrupt:
         logging.info('----------------------- Service Stopped -----------------------')
