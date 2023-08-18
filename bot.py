6d0008a32cfee2cc08def9f974b73acce79cb7fe
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-18 19:07:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,75 +1,128 @@
-# (c) adarsh-goel
-import os
-import sys
-import glob
-import asyncio
 import logging
-import importlib
-from pathlib import Path
-from pyrogram import idle
-from lazybot import StreamBot
-from info import *
-from aiohttp import web
-from plugins import web_server
-from util.keepalive import ping_server
-from lazybot.clients import initialize_clients
+import logging.config
 
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import *
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from aiohttp import web
+from plugins import web_server
 
+import asyncio
+from pyrogram import idle
+from lazybot import StreamBot
+from util.keepalive import ping_server
+from lazybot.clients import initialize_clients
+
+PORT = "8080"
 StreamBot.start()
 loop = asyncio.get_event_loop()
 
-async def start_services():
-    print('\n')
-    print('------------------- Initalizing Telegram Bot -------------------')
-    bot_info = await StreamBot.get_me()
-    StreamBot.username = bot_info.username
-    print("------------------------------ DONE ------------------------------")
-    print()
-    print(
-        "---------------------- Initializing Clients ----------------------"
-    )
-    await initialize_clients()
-    print("------------------------------ DONE ------------------------------")
-    print('\n')
-    print('--------------------------- Importing ---------------------------')
+class Bot(Client):
 
-    if ON_HEROKU:
-        print("------------------ Starting Keep Alive Service ------------------")
+    async def start(self):
+        print('\n')
+        print('------------------- Initalizing Telegram Bot -------------------')
+        bot_info = await StreamBot.get_me()
+        StreamBot.username = bot_info.username
+        print("------------------------------ DONE ------------------------------")
         print()
-        asyncio.create_task(ping_server())
-    print('-------------------- Initalizing Web Server -------------------------')
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0" if ON_HEROKU else BIND_ADRESS
-    await web.TCPSite(app, bind_address, PORT).start()
-    print('----------------------------- DONE ---------------------------------------------------------------------')
-    print('\n')
-    print('---------------------------------------------------------------------------------------------------------')
-    print('---------------------------------------------------------------------------------------------------------')
-    print(' follow me for more such exciting bots! https://github.com/adarsh-goel')
-    print('---------------------------------------------------------------------------------------------------------')
-    print('\n')
-    print('----------------------- Service Started -----------------------------------------------------------------')
-    print('                        bot =>> {}'.format((await StreamBot.get_me()).first_name))
-    print('                        server ip =>> {}:{}'.format(bind_address, PORT))
-    print('                        Owner =>> {}'.format((OWNER_USERNAME)))
-    if ON_HEROKU:
-        print('                        app runnng on =>> {}'.format(FQDN))
-    print('---------------------------------------------------------------------------------------------------------')
-    print('Give a star to my repo https://github.com/adarsh-goel/filestreambot-pro  also follow me for new bots')
-    print('---------------------------------------------------------------------------------------------------------')
-    await idle()
+        print(
+            "---------------------- Initializing Clients ----------------------"
+        )
+        await initialize_clients()
+        print("------------------------------ DONE ------------------------------")
+        print('\n')
+
+        if ON_HEROKU:
+            print("------------------ Starting Keep Alive Service ------------------")
+            print()
+            asyncio.create_task(ping_server())
+        print('-------------------- Initalizing Web Server -------------------------')
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        await super().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0" if ON_HEROKU else BIND_ADRESS
+        await web.TCPSite(app, bind_address, PORT).start()
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
+        await idle()
+
+
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped. Bye.")
+    
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                for message in app.iter_messages("pyrogram", 1, 15000):
+                    print(message.text)
+        """
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
+
+
+app = Bot()
 
 if __name__ == '__main__':
     try:
-        loop.run_until_complete(start_services())
+        loop.run_until_complete(app.start())
     except KeyboardInterrupt:
         logging.info('----------------------- Service Stopped -----------------------')
+
