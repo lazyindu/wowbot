66bb19de99dcf41a6fadf8c1b3ab3c0d1652eab3
indu <70087467+lazyindu@users.noreply.github.com>
2023-08-18 09:37:51 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -6,6 +6,7 @@ import re
 import math
 import logging
 import secrets
+import time
 import mimetypes
 from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
@@ -13,6 +14,7 @@ from lazybot import multi_clients, work_loads, StreamBot
 from server.exceptions import FIleNotFound, InvalidHash
 from zzint import StartTime, __version__
 from util.custom_dl import ByteStreamer
+from util.time_format import get_readable_time
 from util.render_template import render_page
 from info import *
 
@@ -21,8 +23,21 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("in & as LazyDeveloper...")
-
+    return web.json_response(
+        {
+            "server_status": "running",
+            "uptime": get_readable_time(time.time() - StartTime),
+            "telegram_bot": "@" + StreamBot.username,
+            "connected_bots": len(multi_clients),
+            "loads": dict(
+                ("bot" + str(c + 1), l)
+                for c, (_, l) in enumerate(
+                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
+                )
+            ),
+            "version": __version__,
+        }
+    )
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
 async def stream_handler(request: web.Request):
